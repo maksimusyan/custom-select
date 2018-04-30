6665c31f845944c652e8867d89c2b227e9e1ef5f
maksimusyan <etJSQKRT7pcg-5Fn?L>
2018-04-30 22:32:10 +0300

    Найтроен Webpack и создана демо-страница

--- src/js/custom_select.js
+++ src/js/custom_select.js
@@ -1,9 +1,16 @@
-/*!
- * Custom Select v1.0.0
- */
-class CustomSelect{
+class CustomSelectClass{
     constructor(target,options){
+        this._classInit = typeof target === 'object' && typeof target.attributes === 'object';
+        this._opts = {
 
+        };
+        if(typeof options === 'object' && Object.keys(options).length > 0){
+            for (let key in options) {
+                if (options.hasOwnProperty(key)){
+                    this._opts[key] = options[key];
+                }
+            }
+        }
     }
 }
-export default new CustomSelect();
+global.CustomSelect = CustomSelectClass;
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,62 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    entry: './src/js/custom_select.js',
+    mode: "production", // Режим работы Webpack. Доступно: "production" | "development" | "none"
+    entry: ['./src/js/custom_select.js','./src/scss/custom_select.scss'],
     output: {
+        // Целевой каталог для всех выходных файлов
+        // должен быть абсолютным путем (используйте модуль пути Node.js)
         path: path.resolve(__dirname, './build'),
+        // URL-адрес выходного каталога, относительно HTML-страницы
         publicPath: '/build/',
+        // Имя итогового файла
         filename: 'custom_select.min.js'
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        }]
-    }
+        rules: [
+            {
+                // Компиляция sass/scss в css
+                test: /\.(css|sass|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                js: {
+                    test: /\.js$/,
+                    name: 'commons',
+                    chunks: 'all',
+                    minChunks: 7,
+                },
+                css: {
+                    test: /\.(css|sass|scss)$/,
+                    name: 'commons',
+                    chunks: 'all',
+                    minChunks: 2,
+                }
+            }
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'custom_select.css',
+        }),
+    ]
 };
