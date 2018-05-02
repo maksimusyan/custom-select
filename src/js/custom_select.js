a7167ca25d2c77bc210e8b78f00886c7e175efed
maksimusyan <etJSQKRT7pcg-5Fn?L>
2018-05-02 03:30:10 +0300

    Завершёны основные функции JS-класса

--- src/js/custom_select.js
+++ src/js/custom_select.js
@@ -5,21 +5,27 @@ export default class CustomSelectClass
         this._isLoaded = false;
         // Маркер корректно переданного DOM-объекта
         this._targetDomElement = typeof target === 'object' && typeof target.attributes === 'object' ? target : null;
+        // Текущее значение выпадающего списка
         this._currentValues = [];
+        // Создаём основные элементы списка
         this._wrap = document.createElement('div');
         this._head = document.createElement('div');
         this._list = document.createElement('ul');
+        // В основного родителя в качестве произвольного аттрибута добавляем ссылку на текущий экземпляр класса
+        // это необходимо чтобы из стандартного Event при клике достать именно текущий экземпляр класса
+        this._wrap.thisClass = this;
         // Базовые параметры
         this._opts = {
-            data: {},
-            defaultValue: 'none',
-            defaultName: '--- выберите из списка ---',
-            maxWidthHead: 300,
-            maxWidthList: 300,
-            maxHeightList: 600,
-            scrollX: 'hidden',
-            scrollY: 'auto',
-            onChange: function(){},
+            data: {}, // Объект javascript на основе которого будет построен список
+            defaultValue: 'none', // Значение по-умолчанию (оно же - первый элемент списка)
+            defaultName: '--- выберите из списка ---', // Имя по-умолчанию (оно же - первый элемент списка)
+            maxWidthHead: 300, // Максимальная ширина заголовка списка
+            maxWidthList: 300, // Максимальная ширина списка
+            maxHeightList: 600, // Максимальная высота списка
+            scrollX: 'hidden', // Метод отображения горизонтального скролла
+            scrollY: 'auto', // Метод отображения вертикального скролла
+            onChange: function(){}, // callback-функция, которая будет вызвана при выборе элемента выпадающего списка
+            // Базовые имена атрибута "class" элементов списка
             classes: {
                 wrapper: 'custom_select_wrapper',
                 head: 'csw_header',
@@ -47,15 +53,11 @@ export default class CustomSelectClass
         if(this._targetDomElement === null || this._isLoaded){
             return false;
         }
-
         let firstItemName = '', itemCounter = 1;
-
+        // Добавляем классы к основным элементам
         this._wrap.setAttribute('class',this._opts.classes.wrapper);
         this._head.setAttribute('class',this._opts.classes.head);
-
-        this._list.setAttribute('class',this._opts.classes.list);
-        this._list.setAttribute('style','display:none;');
-
+        this._list.setAttribute('class',this._opts.classes.list+' hidden');
         // Добавляем обработчик события клика по странице (для закрытия выпадающего списка)
         document.addEventListener('click', this.onClickDocument);
         // Добавляем обработчик события клика по заголовку выпадающего списка
@@ -64,11 +66,16 @@ export default class CustomSelectClass
         // Добавляем в выпадающий список первый пункт, который устанавливается как "по-умолчанию"
         // такой подход необходим из-за принудительной сортировки объектов браузером
         if(typeof this._opts.defaultValue === 'string' && typeof this._opts.defaultName === 'string'){
+            // Создаём новый элемент списка
             let listItem = document.createElement('li');
             listItem.setAttribute('class',this._opts.classes.listItem);
             listItem.setAttribute('data-value',this._opts.defaultValue);
             listItem.innerHTML = this._opts.defaultName;
+            // Добавляем обработчик события клика по элементу списка
+            listItem.addEventListener('click', this.onClickListItem);
+            // Вставляем элемент в общий список
             this._list.appendChild(listItem);
+            // Заполняем значение данными по-умолчанию
             this._currentValues = [this._opts.defaultValue];
             firstItemName = this._opts.defaultName;
         }
@@ -79,11 +86,17 @@ export default class CustomSelectClass
             for (let keyData in this._opts.data) {
                 if (this._opts.data.hasOwnProperty(keyData)){
                     if(typeof keyData === 'string' && typeof this._opts.data[keyData] === 'string'){
+                        // Создаём новый элемент списка
                         let listItem = document.createElement('li');
                         listItem.setAttribute('class',this._opts.classes.listItem);
                         listItem.setAttribute('data-value',keyData);
                         listItem.innerHTML = this._opts.data[keyData];
+                        // Добавляем обработчик события клика по элементу списка
+                        listItem.addEventListener('click', this.onClickListItem);
+                        // Вставляем элемент в общий список
                         this._list.appendChild(listItem);
+                        // Если не были указаны значения по-умолчанию,
+                        // то заполняем текущее значение первым элементом в списке
                         if(itemCounter === 1 && firstItemName === ''){
                             this._currentValues = [keyData];
                             firstItemName = this._opts.data[keyData];
@@ -93,27 +106,70 @@ export default class CustomSelectClass
                 }
             }
         }
+        // Добавляем DOM-элементы на страницу
         this._head.innerHTML = firstItemName;
-
         this._wrap.appendChild(this._head);
         this._wrap.appendChild(this._list);
         this._targetDomElement.appendChild(this._wrap);
-
+        // Ставим маркер успешной инициации
         this._isLoaded = true;
     }
     onClickDocument(event){
-
+        /** TODO: в будущем нужно учитывать переназначенные классы экземпляров custom_select */
+        // Если клик не по документу, а по по экземляру custom_select = прерываем выполнение события
+        if(event.target.parentNode !== document){
+            if(event.target.parentNode.classList.contains('custom_select_wrapper') || event.target.parentNode.classList.contains('csw_list')){
+                return;
+            }
+        }
+        // Ищем все экземляры custom_select и закрываем выпадающий список
+        let customSelects = document.getElementsByClassName('csw_list');
+        if(customSelects !== null){
+            // Если экземляры custom_select найдены
+            Array.prototype.forEach.call(customSelects, function(item) {
+                // Если экземляры custom_select не имеют класса hidden
+                if(!item.classList.contains('hidden')){
+                    item.classList.add('hidden');
+                }
+            });
+        }
     }
     onClickHead(event){
-        //console.log(self._list);
+        // При клике по заголовку custom_select открываем выпадающий список
+        let _list = event.target.nextSibling;
+        if(_list !== null){
+            _list.classList.remove('hidden');
+        }
     }
-    onClickListItem(){
-
+    onClickListItem(event){
+        // При клике по элементу выпадающего списка выполняем действия:
+        // 1. Устанавливаем переменные для работы
+        let thisClass = event.target.parentNode.parentNode.thisClass,
+            currentName = event.target.textContent,
+            currentValue = event.target.getAttribute('data-value'),
+            _head = event.target.parentNode.previousSibling;
+        // 2. Меняем заголовок списка на название выбранного элемента
+        if(_head !== null){
+            _head.innerHTML = currentName;
+        }
+        // 3. Устанавливаем значение выбранного элемента в качестве значения объекта
+        thisClass._currentValues = [currentValue];
+        // 4. Скрываем список
+        if(!event.target.parentNode.classList.contains('hidden')){
+            event.target.parentNode.classList.add('hidden');
+        }
+        // 5. Вызываем callback-функцию
+        thisClass.onChange();
     }
     onChange(){
         if(typeof this._opts.onChange === 'function'){
             this._opts.onChange(this._currentValues);
         }
     }
+    getValues(){
+        // Возвращаем текущее значение
+        return this._currentValues;
+    }
 }
+// Устанавливаем CustomSelect как глобальную переменную для прямого обращения к классу из любого места
 global.CustomSelect = CustomSelectClass;
